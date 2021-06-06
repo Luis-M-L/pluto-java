f5cfabb76b9dcf84eaba505fc16bef13d489b2ed
LuisML <luismtinezlopez@gmail.com>
2021-06-06 19:14:01 +0200

    Initializing and injecting repository bean

--- persistencia/src/main/java/com/example/pluto/persistencia/PersistenciaService.java
+++ persistencia/src/main/java/com/example/pluto/persistencia/PersistenciaService.java
@@ -3,27 +3,20 @@ package com.example.pluto.persistencia;
 import com.example.pluto.entities.SpotTO;
 import com.example.pluto.repositories.SpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PersistenciaService {
 
-    private static Map<Class, CrudRepository> repositoryFactory;
-/*
     @Autowired
-    private SpotRepository spotRepository;
-*/
+    SpotRepository spotRepository;
+
     public PersistenciaService() {
-        repositoryFactory = new HashMap<>();
-        //repositoryFactory.put(SpotTO.class, spotRepository);
     }
 
-    public void save(Object spot) {
-        repositoryFactory.get(spot).save(spot);
+    public void save(Object obj) {
+        SpotTO spot = (SpotTO) obj;
+        spotRepository.save(spot);
     }
 
 }
