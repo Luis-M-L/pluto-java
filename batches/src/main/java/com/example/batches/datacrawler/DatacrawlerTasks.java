7ca449ca0c4b202abb4c7f1405e537af77797bcd
LuisML <luismtinezlopez@gmail.com>
2021-07-17 12:27:38 +0200

    Datacrawler adapted to get ccy list from persistence service

--- batches/src/main/java/com/example/batches/datacrawler/DatacrawlerTasks.java
+++ batches/src/main/java/com/example/batches/datacrawler/DatacrawlerTasks.java
@@ -1,5 +1,8 @@
 package com.example.batches.datacrawler;
 
+import com.example.pluto.entities.InstrumentTO;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -10,7 +13,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -21,11 +24,23 @@ public class DatacrawlerTasks {
 
     public static void registerSpots(){
         LOG.info("Registering spot data");
-        getParesVigilados().forEach(par -> requestSave(getSpot(par)));
+        getParesVigilados().forEach(par -> requestSave(getSpot(par.getTicker())));
     }
 
-    private static List<String> getParesVigilados(){
-        return Arrays.asList("BTCUSD", "ETHUSD", "ETHBTC");
+    private static List<InstrumentTO> getParesVigilados(){
+        HttpRequest request = HttpRequest.newBuilder(URI.create("http://persistencia:8080/persistence/instruments/")).build();
+        HttpResponse<String> response = null;
+        List<InstrumentTO> vigilados = new ArrayList<>();
+        try {
+            response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            vigilados = new ObjectMapper().readValue(response.body(), new TypeReference<List<InstrumentTO>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return vigilados;
     }
 
     private static byte[] getSpot(String par){
