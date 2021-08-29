6a719eb879040e9e4025bd36dac19d24b8f52fef
LuisML <luismtinezlopez@gmail.com>
2021-08-29 18:32:20 +0200

    Several baskets for equivalent test

--- batches/src/test/java/com/example/batches/assetmanager/AssetManagerTasksTest.java
+++ batches/src/test/java/com/example/batches/assetmanager/AssetManagerTasksTest.java
@@ -32,7 +32,7 @@ public class AssetManagerTasksTest {
         spotsMap.put("ETH", 0.11);
         spotsMap.put("BCH", 0.019);
         spotsMap.put("XMR", 0.005);
-        spotsMap.put("ADA", 0.0000441);
+        spotsMap.put("ADA", 0.00004);
         spotsMap.put("IOT", 0.0000215);
 
         positions = new ArrayList<>(3);
@@ -44,13 +44,33 @@ public class AssetManagerTasksTest {
         BasketTO basketBasica = new BasketTO(9L, "basica", weightsBasica);
         positions.add(new PositionTO(1L, basketBasica, "BTC", 0.5));
         positions.add(new PositionTO(2L, basketBasica, "ETH", 0.1));
-        positions.add(new PositionTO(2L, basketBasica, "XMR", 0.033));
+        positions.add(new PositionTO(3L, basketBasica, "XMR", 0.033));
+
+        List<WeightTO> weightsGrowth = Arrays.asList(
+                new WeightTO(null, "BTC", 0.15, null),
+                new WeightTO(null, "ETH", 0.2, null),
+                new WeightTO(null, "XMR", 0.25, null),
+                new WeightTO(null, "ADA", 0.3, null),
+                new WeightTO(null, "IOT", 0.1, null)
+        );
+        BasketTO basketGrowth = new BasketTO(8L, "growth", weightsGrowth);
+        positions.add(new PositionTO(4L, basketGrowth, "IOT", 4650.0));
+        positions.add(new PositionTO(5L, basketGrowth, "ETH", 1.82));
+        positions.add(new PositionTO(6L, basketGrowth, "XMR", 50.0));
+        positions.add(new PositionTO(7L, basketGrowth, "ADA", 6800.0));
+        positions.add(new PositionTO(8L, basketGrowth, "BTC", 0.15));
 
         equivalent = new HashMap<>(2);
-        equivalent.put(basketBasica, new HashMap<String, Double>());
+        equivalent.put(basketBasica, new HashMap<>());
         equivalent.get(basketBasica).put("BTC", 0.5);
         equivalent.get(basketBasica).put("ETH", 0.011);
-        equivalent.get(basketBasica).put("XMR", 0.033*0.005);
+        equivalent.get(basketBasica).put("XMR", 0.000165);
+        equivalent.put(basketGrowth, new HashMap<>());
+        equivalent.get(basketGrowth).put("BTC", 0.15);
+        equivalent.get(basketGrowth).put("ADA", 0.272);
+        equivalent.get(basketGrowth).put("XMR", 0.25);
+        equivalent.get(basketGrowth).put("ETH", 0.2002);
+        equivalent.get(basketGrowth).put("IOT", 0.099975);
     }
 
     @Test
