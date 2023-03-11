fbc0d3f914e16fa9855083065fcd09abc9eb9d0d
LuisML <luismtinezlopez@gmail.com>
2023-03-11 14:16:31 +0100

    Logging for datacrawler

--- datacrawler/datacrawler.py
+++ datacrawler/datacrawler.py
@@ -1,15 +1,19 @@
 import requests
 import json
+import logging
+
+logging.basicConfig(filename='logs/datacrawler.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
 def get_instruments():
  r = requests.get('http://127.0.0.1:48557/instruments')
+ logging.debug("Got instruments to watch: " + r.text)
  return r.json()
 
 def request_save(instrument):
  url = 'http://127.0.0.1:48558/bitfinex/spot/' + instrument
- print(url)
+ logging.debug("Requesting to save: " + url)
  r = requests.post(url)
- print(r.text)
+ logging.debug("Success when saving? : " + r.text)
 
 def crawling():
  instruments = get_instruments()
